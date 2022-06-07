f3d84499dfbc8ab7dae0f2b1dec455ee17657f1b
ChemaSalazar <ChemaSalazar@Outlook.com>
2022-06-07 16:45:33 -0500

    Update to image gallery container

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -2,8 +2,10 @@
   <div v-editable="blok">
     <h2>{{ blok.title }}</h2>
   </div>
-  <div v-editable="blok" class="flex py-8 mb-6">
-    <div v-for="blok in blok.images" :key="blok._uid" class="flex-auto px-6">
+  <!-- <div v-editable="blok" class="flex py-8 mb-6"> -->
+  <div v-editable="blok" class="lg:columns-3 md:columns-2 sm:columns-1">
+    <!-- <div v-for="blok in blok.images" :key="blok._uid" class="flex-auto px-6"> -->
+    <div v-for="blok in blok.images" :key="blok._uid" class="">
       <StoryblokComponent :blok="blok" />
     </div>
   </div>
